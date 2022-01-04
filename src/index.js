be0357b52a2186717750e4635403b3f6538c4bff
Anton <antonogonovskiy@gmail.com>
2022-01-04 18:58:01 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+	let strNum = String(n);
+	let result = '';
+	for (let i = 0; i < strNum.length; i += 1) {
+		if (strNum[i] === '-') {
+			result = result;
+		} else {
+			result = `${strNum[i]}${result}`;
+		}
+	};
+	return Number(result);
 }
